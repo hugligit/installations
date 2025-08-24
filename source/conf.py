67ab6f25276b07c5612c4e2bd7702b6644c012de
Marcel Hrdina <marcel.hrdina@gmail.com>
2025-08-24 21:36:37 +0100

    change the theme to rtd

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,9 @@ author = 'Marcel'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+        'sphinx_rtd_theme'
+        ]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,4 +26,5 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
